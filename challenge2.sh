5fde07a438d21112e6c7dbb848c8bb9cc222df61
gopaka <arungopakumar0@gmail.com>
2022-12-29 01:26:21 +0530

    adding all the challenges

---
+++ challenge2.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+
+echo Kindly enter the username and passord below
+
+read -p 'username :' username
+read -sp 'password :' password
+
+echo Your username is $username and password is $password
+echo Your bash version is $BASH_VERSION 
+echo Your bash shell is `which bash`
+echo Your home dir is $HOME and Your current directory is `pwd`
+
+echo Please give 4 names to store in array
