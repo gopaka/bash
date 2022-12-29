141ed80a39c5973572ccd013e34107eb8fe2edca
gopaka <arungopakumar0@gmail.com>
2022-12-29 23:25:03 +0530

    removed unwanted parameter for alert_api script ( echo -e) -e is removed and not required

--- alert_api.sh
+++ alert_api.sh
@@ -1,13 +1,13 @@
-echo -e "\n"
-echo This is a bash script can run in any linux machine have access to the cluster ip
+echo "\n"
+echo This is a bash script can run from any linux machine have access to the cluster ip
 echo change ip in ips based on the cluster ip we need to add notification alerts
 echo keep alert1.json, alert2.json,... in a single directory
-echo -e "\n"
+echo "\n"
 echo If error is "KInternalError","message":"An internal critical error has occurred." then alert is already present in the notification
-echo -e "\n"
+echo "\n"
 echo Script starts here
 echo ==========================
-echo -e "\n"
+echo "\n"
 
 ips=("10.16.16.173" "10.16.18.25")
 
@@ -27,6 +27,6 @@ for ip in ${ips[@]} ; do
 		echo "Adding alert rule ID " $ruleId
 		curl --insecure -X POST "https://$ip/irisservices/api/v1/public/alertNotificationRules" -H "accept: application/json" -H "Content-Type: application/json" -H "authorization : Bearer $token" -d @${alert_file}
 
-echo -e "\n"
+echo  "\n"
 	done
 done
